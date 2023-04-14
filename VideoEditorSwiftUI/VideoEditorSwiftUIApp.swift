298bb033af28eda3cdac63bc7e57825b420c8b55
Bogdan Zyk <zykovbogdan07@gmail.com>
2023-04-14 11:23:26 +0300

    Initial Commit

---
+++ VideoEditorSwiftUI/VideoEditorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorSwiftUIApp.swift
+//  VideoEditorSwiftUI
+//
+//  Created by Bogdan Zykov on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
